d4a7764ca16d3a7c8f73d14512e7ef0fbe50f553
Bruno Jimenez Postali <bruno.postali@gmail.com>
2022-05-30 22:48:54 -0300

    added new styles on all components

--- src/nav-mobile.js
+++ src/nav-mobile.js
@@ -1,8 +1,26 @@
 const btnMobile = document.getElementById('btn-mobile')
 btnMobile.addEventListener('click', toggleMenu)
 
+const closeMenu = document.getElementById('close-menu')
+closeMenu.addEventListener('click', toggleMenu)
+
 function toggleMenu() {
 
   const nav = document.getElementById('nav')
   nav.classList.toggle('active')
+}
+
+function toggleModal() {
+  
+  const modal = document.getElementById('bg')
+  modal.classList.toggle('active')
+ 
+  if (nav.classList.value === 'active') {
+    nav.classList.toggle('active')
+  }
+}
+
+function closeModal() {
+  const modal = document.getElementById('bg')  
+  modal.classList.toggle('active')
 }
