121fa5a29f4636ee23fc1bf3a4b57a11d9ea17e3
Bruno Jimenez Postali <bruno.postali@gmail.com>
2022-06-04 16:44:31 -0300

    added new data info

--- src/index.js
+++ src/index.js
@@ -1,26 +1,66 @@
-const colors = ['Red', 'Green', 'Blue']
-const getMethodMessage = document.getElementById('response')
 const data = [
     {
         id: "1",
         title: "<span style='color:#d4445c; font-size:40px; font-weight:bold'>array.pop()</span><br><br>",
         description: "Este método é utilizado para <strong style='color: #d4445c'>remover</strong> o último elemento do array.<br><br>",
-        example: "Considere o array: colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br><br>",
+        example: "Considere o array: colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br>",
         resolution: "Ao fazer uso do método 'colors.pop()' o array ficará: <br><br> colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>]"
     },
     {
         id: "2",
-        title: "<span style='color:#d4445c; font-size:40px; font-weight:bold'>array.push()</span><br><br>",
-        description: "Este método é usado para <strong style='color: #d4445c'>adicionar</strong> um elemento no final do array.<br>",
-        example: "Considere o array: colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br><br>",
+        title: "<span style='color:#d4445c; font-size:40px; font-weight:bold'>array.push(param)</span><br><br>",
+        description: "Este método é usado para <strong style='color: #d4445c'>adicionar</strong> um elemento no final do array.<br><br>",
+        example: "Considere o array: colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br>",
         resolution: "Ao fazer uso do método 'colors.push(`Orange`)' o array ficará: <br><br> colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>, <span style='color: orange'>Orange</span>]"
-    }   
+    },
+    {
+        id: "3",
+        title: "<span style='color:#d4445c; font-size:40px; font-weight:bold'>array.shift()</span><br><br>",
+        description: "Este método é usado para <strong style='color: #d4445c'>remover</strong> um elemento no início do array.<br><br>",
+        example: "Considere o array: colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br>",
+        resolution: "Ao fazer uso do método 'colors.shift()' o array ficará: <br><br> colors = [<span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]"
+    },
+    {
+        id: "4",
+        title: "<span style='color:#d4445c; font-size:40px; font-weight:bold'>array.unshift(param)</span><br><br>",
+        description: "Este método é usado para <strong style='color: #d4445c'>adicionar</strong> um elemento no início do array.<br><br>",
+        example: "Considere o array: colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br>",
+        resolution: "Ao fazer uso do método 'colors.unshift(`Orange`)' o array ficará: <br><br> colors = [<span style='color: orange'>Orange</span>, <span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]"
+    },
+    {
+        id: "5",
+        title: "<span style='color:#d4445c; font-size:40px; font-weight:bold'>array.concat(param)</span><br><br>",
+        description: "Este método é usado para <strong style='color: #d4445c'>concatenar</strong> dois ou mais arrays distintos.<br><br>",
+        example: "Considere os arrays: colorsA = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br> e colorsB = [<span style='color:orange'>Orange</span>, <span style='color:purple'>Purple</span>]<br>",
+        resolution: "Ao fazer uso do método const 'newColors = colorsA.concat(colorsB)' o array ficará: <br><br> newColors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>, <span style='color: orange'>Orange</span>, <span style='color: purple'>Purple</span>]"
+    },
+    {
+        id: "6",
+        title: "<span style='color:#d4445c; font-size:40px; font-weight:bold'>array.splice(param¹, param², param³...)</span><br><br>",
+        description: "Este método é usado para <strong style='color: #d4445c'>adicionar</strong> elementos ao array.<br><br>O <span style='color:#d4445c'>param¹</span> representa a posição no array, aonde o(s) item(s) serão inserido(s).<br>O <span style='color:#d4445c'>param²</span> representa quantos elementos serão removidos do array, a partir da nova inserção.<br>O <span style='color:#d4445c'>param³</span> representa o(s) item(s) a inserir no array.<br><br>",
+        example: "Considere o array: colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br>",
+        resolution: "Ao fazer uso do método 'colors.splice(2, 0, 'Orange', 'Purple')' o array ficará: <br><br> colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: orange'>Orange</span>, <span style='color: purple'>Purple</span>, <span style='color: blue'>Blue</span>]<br><br> <span style='font-size:20px; color:#d4445c'>``Lembrando que você poderá usar este método para remover elemtos de indíce específicos, basta por exemplo<br> fazer um 'array.splice(1, 2)' - Irá remover 2 elementos apartir do index 1.´´</span>"
+    },
+    {
+        id: "7",
+        title: "<span style='color:#d4445c; font-size:40px; font-weight:bold'>array.slice(param)</span><br><br>",
+        description: "Este método é usado para <strong style='color: #d4445c'>criar</strong> um novo array, apartir do index indicado.<br><br>",
+        example: "Considere o array: colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br>",
+        resolution: "Ao fazer uso do método 'newColors = colors.slice(1)' o array ficará: <br><br> newColors = [<span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br>"
+    },
+    {
+        id: "8",
+        title: "<span style='color:#d4445c; font-size:40px; font-weight:bold'>array.toString()</span><br><br>",
+        description: "Este método é usado para <strong style='color: #d4445c'>transformar</strong> o array em uma string, separada por virgulas.<br><br>",
+        example: "Considere o array: colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br>",
+        resolution: "Ao fazer uso do método 'colors.toString()' o array ficará: <br><br> Red,Green,Blue<br>"
+    },
 ]
 
-
+const getMethodMessage = document.getElementById('response')
 
 function insertMethods(id) {
-    toggleResponse()
+    toggleResponse()    
     getMethodMessage.innerHTML = `
     ${data[id - 1].title}
     ${data[id - 1].description}
@@ -28,56 +68,4 @@ function insertMethods(id) {
     ${data[id - 1].resolution}
     `
 }
-
-
-/*
-function arrayUnshift() {
-    colors.unshift('Orange')
-    getMethodMessage.innerHTML = `
-    Considere o array: colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br><br>
-    Este método é usado para <strong style='color: #d4445c'>adicionar</strong> um elemento ao seu array, em seu primeiro indíce, criando um novo array.<br>
-    Vamos adicionar 'Orange' ao array:
-    <br> <i>colors.unshift('Orange')</i>
-    <br>Agora o array é: 
-    ${
-        "<span style='color: orange'>" + colors[0] + "</span>, " +
-        "<span style='color: red'>" + colors[1] + "</span>, " +
-        "<span style='color: green'>" + colors[2] + "</span>, " +
-        "<span style='color: blue'>" + colors[3] + "</span>"
-    } 
-    ` 
-    typeWriter(getMethodMessage)
-    colors.shift()
-}
-
-function arrayShift() {
-    getMethodMessage.innerHTML = `
-    Considere o array: colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br><br>
-    Este método é utilizado para <strong style='color: #d4445c'>remover</strong> o primeiro elemento do array:
-    <br> <i>colors.shift()</i>
-    <br>Elemento removido: <strong style='color: red'>${colors.shift()}</strong>
-    <br>Agora o array é: 
-    ${
-        "<span style='color: green'>" + colors[0] + "</span>, " +
-        "<span style='color: blue'>" + colors[1] + "</span>"
-    } 
-    ` 
-    colors.unshift("Red")  
-}
-
-function arrayJoin() {
-    getMethodMessage.innerHTML = `
-    Considere o array: colors = [<span style='color: red'>Red</span>, <span style='color: green'>Green</span>, <span style='color: blue'>Blue</span>]<br><br>
-    Este método é usado para <strong style='color: #d4445c'>transformar</strong> o seu array em uma string, podendo acrescentar carácteres aos arrays.<br>
-    Vamos adicionar '*' em cada elemento no array:
-    <br> <i>colors.join('*')</i>
-    <br>Agora o array é: 
-    ${
-        "<span style='color: red'>" + colors[0] + "</span> * " +
-        "<span style='color: green'>" + colors[1] + "</span> * " +
-        "<span style='color: blue'>" + colors[2] + "</span> * "
-    } 
-    ` 
-    console.log(colors.join('*'))
-}*/
 
